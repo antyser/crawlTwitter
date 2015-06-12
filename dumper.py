effcf0b7e25e12e85058583ddd17e5e0078c8ca9
Jun Liu <antyser@gmail.com>
2015-06-12 10:55:17 -0700

    1

--- dumper.py
+++ dumper.py
@@ -1,19 +1,27 @@
 __author__ = 'junliu'
-import datetime, sys
+import time, sys
 from time import gmtime, strftime
 import kafkaUtil
+import json
 
 def consume(kafka_host, topic, filename):
-    consumer = kafkaUtil.create_consumer(kafka_host, "172.31.24.55:2181", topic, strftime("%Y%m%d%H%M%S", gmtime()))
-    while True:
-        with open(filename, 'a') as out:
+    consumer = kafkaUtil.create_consumer(kafka_host, "172.31.24.55:2181", topic, "monitor")
+    current_timestamp = int(time.time())
+    counter = 0
+    with open(filename, 'a') as out:
+        while True:
             msg = consumer.consume()
             if msg is None:
                 continue
+            jsonobj = json.loads(msg)
+            if jsonobj['ts_fetch'] > current_timestamp:
+                break
             out.write(msg.value)
             out.write('\n')
+            counter += 1
+        out.write("till " + str(current_timestamp), "consume " + str(counter))
 
 
 if __name__ == '__main__':
-    print 'usage: python extractUrl.py <kafka-host:port> <topic> <filename>'
+    print 'usage: python dumper.py <kafka-host:port> <topic> <filename>'
     consume(sys.argv[1], sys.argv[2], sys.argv[3])
