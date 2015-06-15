21ebb3d78b873a9a4c23281e2c533d0048b16c41
Jun Liu <antyser@gmail.com>
2015-06-15 13:38:09 -0700

    add fetchTopsite

--- fetchTopSite.py
+++ fetchTopSite.py
@@ -2,45 +2,40 @@ from kafka import SimpleProducer, KafkaClient, SimpleConsumer
 from kafka.common import MessageSizeTooLargeError
 import json, sys, requests, logging, time
 
-
 HEADERS = {'User-agent': 'Mozilla/5.0'}
+
 
 def fetchUrl(url, producer):
     r = requests.get(url, headers=HEADERS)
-    logging.info('fetch: '+url)
+    logging.info('fetch: ' + url)
 
     if r.status_code == requests.codes.ok:
-        page = {'seed': url, 'data': r.text, 'ts_task': int(time.time()), 'label': 'toppage'}
+        page = {}
+        page['url'] = url
+        page['content'] = r.text
+        page['ts_fetch'] = int(time.time())
         try:
-            producer.send_messages("seeds", json.dumps(page))
+            producer.send_messages("toppage.pages", json.dumps(page))
         except MessageSizeTooLargeError as err:
             logging.warning(err)
     else:
-        logging.warning(str(r.status_code)+'\t'+ r.reason)
+        logging.warning(str(r.status_code) + '\t' + r.reason)
 
 
 def fetchFrom(kafka_host):
     kafka = KafkaClient(kafka_host)
-    consumer = SimpleConsumer(kafka, 'fetcher', 'topsite.links')
+    consumer = SimpleConsumer(kafka, 'tmp', 'topsite.pages')
     producer = SimpleProducer(kafka)
 
     for msg in consumer:
-        topsites = json.loads(msg.message.value)
-        for url in topsites['links']:
-            seed = {}
-            seed['url'] = url
-            seed['ts_task'] = int(time.time())
-            seed['label'] = 'toppage'
-            try:
-                producer.send_messages("seeds", json.dumps(seed))
-            except MessageSizeTooLargeError as err:
-                logging.warning(err)
+        obj = json.loads(msg.message.value)
+        for url in obj['links']:
+            fetchUrl(url, producer)
 
     kafka.close()
 
 
 if __name__ == '__main__':
-    print 'USAGE:  python fetchTopSite.py'
     logging.basicConfig(file='fetch.log', level=logging.INFO)
 
     kafka_host = '172.31.10.154:9092'
