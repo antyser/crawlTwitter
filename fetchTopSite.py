844dfaa0d473ca80e177be5f6db0e67fed66ec3e
Jun Liu <antyser@gmail.com>
2015-06-15 09:20:43 -0700

    add fetchTopSite.py

---
+++ fetchTopSite.py
@@ -0,0 +1,42 @@
+from kafka import SimpleProducer, KafkaClient, SimpleConsumer
+from kafka.common import MessageSizeTooLargeError
+import json, sys, requests, logging, time
+
+
+HEADERS = {'User-agent': 'Mozilla/5.0'}
+
+def fetchUrl(url, producer):
+    r = requests.get(url, headers=HEADERS)
+    logging.info('fetch: '+url)
+
+    if r.status_code == requests.codes.ok:
+        page = {'seed': url, 'data': r.text, 'ts_task': int(time.time()), 'label': 'toppage'}
+        try:
+            producer.send_messages("seeds", json.dumps(page))
+        except MessageSizeTooLargeError as err:
+            logging.warning(err)
+    else:
+        logging.warning(str(r.status_code)+'\t'+ r.reason)
+
+
+def fetchFrom(kafka_host):
+    kafka = KafkaClient(kafka_host)
+    consumer = SimpleConsumer(kafka, 'fetcher', 'topsite.links')
+    producer = SimpleProducer(kafka)
+
+    for msg in consumer:
+        topsites = json.loads(msg.message.value)
+        for url in topsites['links']:
+            fetchUrl(url, producer)
+
+
+    kafka.close()
+
+
+if __name__ == '__main__':
+    print 'USAGE:  python fetchTopSite.py'
+    logging.basicConfig(file='fetch.log', level=logging.INFO)
+
+    kafka_host = '172.31.10.154:9092'
+
+    fetchFrom(kafka_host)
